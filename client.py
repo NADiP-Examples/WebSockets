a5d202e67474a2d5446b1691390b9459953bf4ca
BooblegumGit <booblegum42@gmail.com>
2016-02-26 14:30:17 +0700

    попытка-1 встроить pgu в pygame

--- client.py
+++ client.py
@@ -4,44 +4,47 @@ import pygame
 import websocket
 
 RESX = 400
-RESY = 200
+RESY = 400
 FPS = 40
 
 
-def on_message(message):
-    print(message)
+class GuiWindow:
+    def __init__(self):
+        self.app = app = gui.App()
+        button_send = gui.Button('Send')
+        button_OK = gui.Button('OK')
+        # button_send.connect(gui.CLICK, on_btn_click, 'send')
+        self.rect = pygame.Rect((120, 50, 200, 50))
+        table = gui.Table()
+        table.tr()
+        table.td(gui.Label("Button: "))
+        table.td(button_send)
+        table.tr()
+        table.td(gui.Label("Button: "))
+        table.td(button_OK)
+        app.init(widget=table, screen=screen, area=self.rect)
 
+    def event(self, event):
+        self.app.event(event)
 
-def on_btn_click(value):
-    print(value)
-    ws.send("Hello, World")
-    result = ws.recv()
-    print("result = ", result)
+    def paint(self):
+        pygame.draw.rect(screen, (0, 200, 0), self.rect, 2)
+        self.app.paint()
 
 # INIT
 screen = pygame.display.set_mode((RESX, RESY))
-app = gui.App()
+window = GuiWindow()
 
 # SOCKETS
-ws = websocket.create_connection("ws://127.0.0.1:8888/websocket")
-# ws.setblocking(0)
-# ws = websocket.WebSocketApp("ws://127.0.0.1:8888/websocket", on_message=on_message)
-# ws.on_open = ws.run_forever()
 
-button = gui.Button('Send message')
-button.connect(gui.CLICK, on_btn_click, 'OK')
-app.init(widget=button)
-# app.run()
 clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit()
-        else:
-            app.event(event)
+        window.event(event)
     dt = clock.tick(FPS)
-    screen.fill((0, 0, 0))
-    app.paint()
+    screen.fill((0, 100, 0))
+    # screen.blit(place, place.get_rect())
+    window.paint()
     pygame.display.flip()
